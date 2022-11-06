a8ec57cb6c09bc7918c80d5285dddb301f25fbe8
Arman <arman18969@gmail.com>
2022-11-07 01:55:29 +0530

    first commit

---
+++ components/layout/components/HeaderLogo.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const HeaderLogo = () => {
+  return (
+    <Logo>Helper</Logo>
+  )
+}
+
+
+const Logo = styled.h1`
+  font-weight: normal;
+  font-size: 35px;
+  margin-left: 11px;
+  font-family: 'Roboto';
+  letter-spacing: 3px;
+`
+
+
+
+
+export default HeaderLogo
